da44ab38fa58b6255ed0c410681c177577078d1c
nitind <nitind>
2007-10-30 21:15:47 +0000

    This commit was manufactured by cvs2svn to create tag 'v200710311517'.

--- bundles/org.eclipse.wst.jsdt.support.ie/libraries/IE_onafterprint.js
+++
@@ -1,1 +0,0 @@
-/**  * Object onafterprint()  * @type onafterprint  * @super Object  * @class onafterprint  * @since JScript 5.6  * @link http://msdn2.microsoft.com/en-us/library/ms536788.aspx*/function onafterprint(){};onafterprint.prototype= new Object();
--- bundles/org.eclipse.wst.jsdt.support.ie/libraries/IE_ondataavailable.js
+++
@@ -1,1 +0,0 @@
-/**  * Object ondataavailable()  * @type ondataavailable  * @super Object  * @class ondataavailable  * @since JScript 5.6  * @link http://msdn2.microsoft.com/en-us/library/ms536918.aspx*/function ondataavailable(){};ondataavailable.prototype= new Object();
