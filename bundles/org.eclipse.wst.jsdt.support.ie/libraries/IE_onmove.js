438644416dd98c66c3abe4e98e52c5718cd17885
nitind <nitind>
2008-02-27 22:36:09 +0000

    This commit was manufactured by cvs2svn to create tag 'v200802272248'.

--- bundles/org.eclipse.wst.jsdt.support.ie/libraries/IE_onmove.js
+++
@@ -1,1 +0,0 @@
-/**  * Object onmove()  * @type onmove  * @super Object  * @class onmove  * @since JScript 5.6  * @link http://msdn2.microsoft.com/en-us/library/ms536952.aspx*/function onmove(){};onmove.prototype= new Object();
