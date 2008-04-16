e641064204a8ee95d1ba37fd255a957f23904e8a
nitind <nitind>
2008-04-16 23:24:53 +0000

    This commit was manufactured by cvs2svn to create tag 'v200804162336'.

--- bundles/org.eclipse.wst.jsdt.support.ie/libraries/IE_nextpage.js
+++
@@ -1,1 +0,0 @@
-/**  * Object nextPage()  * @type nextPage  * @super Object  * @class nextPage  * @since JScript 5.6  * @link http://msdn2.microsoft.com/en-us/library/ms534188.aspx*/function nextPage(){};nextPage.prototype= new Object();
--- bundles/org.eclipse.wst.jsdt.support.ie/libraries/IE_onfinish.js
+++
@@ -1,1 +0,0 @@
-/**  * Object onfinish()  * @type onfinish  * @super Object  * @class onfinish  * @since JScript 5.6  * @link http://msdn2.microsoft.com/en-us/library/ms536933.aspx*/function onfinish(){};onfinish.prototype= new Object();
