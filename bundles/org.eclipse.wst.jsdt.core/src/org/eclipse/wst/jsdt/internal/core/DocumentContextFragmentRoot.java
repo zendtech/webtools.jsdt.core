ddf9614851ed7795fd59f6ee9141658c3c4fd81c
bchilds <bchilds>
2008-11-21 21:13:20 +0000

    copyright update.

--- bundles/org.eclipse.wst.jsdt.core/src/org/eclipse/wst/jsdt/internal/core/DocumentContextFragmentRoot.java
+++ bundles/org.eclipse.wst.jsdt.core/src/org/eclipse/wst/jsdt/internal/core/DocumentContextFragmentRoot.java
@@ -7,6 +7,7 @@
  *
  * Contributors:
  *     IBM Corporation - initial API and implementation
+ *     bug:244839 - eugene@genuitec.com
  *******************************************************************************/
 package org.eclipse.wst.jsdt.internal.core;
 
