5f20211dedb5afc13d770d69f33de7f549c675e3
nitind <nitind>
2010-08-13 14:57:55 +0000

    [nobug] partial revert of [286195] for compatibility

--- bundles/org.eclipse.wst.jsdt.core/src/org/eclipse/wst/jsdt/libraries/Messages.java
+++ bundles/org.eclipse.wst.jsdt.core/src/org/eclipse/wst/jsdt/libraries/Messages.java
@@ -23,6 +23,10 @@ public class Messages extends NLS {
 
 	public static String BasicBrowserLibraryJsGlobalScopeContainerInitializer_ECMA3DOM;
 
+	/**
+	 * @deprecated
+	 */
+	public static String BasicBrowserLibraryJsGlobalScopeContainerInitializer_Window;
 	static {
 		// initialize resource bundle
 		NLS.initializeMessages(BUNDLE_NAME, Messages.class);
