7f7c081f37ee31909fae3801e649ad6ee6cc0f8b
cjaun <cjaun>
2010-09-28 15:38:49 +0000

    [319879] Completion proposal causes validation warning

--- bundles/org.eclipse.wst.jsdt.core/src/org/eclipse/wst/jsdt/internal/compiler/parser/JavadocParser.java
+++ bundles/org.eclipse.wst.jsdt.core/src/org/eclipse/wst/jsdt/internal/compiler/parser/JavadocParser.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2009 IBM Corporation and others.
+ * Copyright (c) 2000, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -385,6 +385,8 @@ public class JavadocParser extends AbstractCommentParser {
 			Object[] type = parseTypeReference();
 			this.returnType = (TypeReference) (type != null && type.length > 0 ? type[0] : null);
 			this.returnStatement = createReturnStatement();
+			if((this.kind & TEXT_VERIF) != 0 && type != null && type.length > 0 && type[0] != null)
+				refreshReturnStatement();
 			return true;
 		}
 		if (this.reportProblems) {
