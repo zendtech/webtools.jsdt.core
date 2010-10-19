82a5fe01c339b4e49e84dcdbd36695c3eb2d8e16
nitind <nitind>
2010-10-19 17:11:54 +0000

    [326349] [refactoring] Refactoring array name fails

--- bundles/org.eclipse.wst.jsdt.ui/src/org/eclipse/wst/jsdt/internal/corext/refactoring/rename/RenameAnalyzeUtil.java
+++ bundles/org.eclipse.wst.jsdt.ui/src/org/eclipse/wst/jsdt/internal/corext/refactoring/rename/RenameAnalyzeUtil.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2009 IBM Corporation and others.
+ * Copyright (c) 2000, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -406,6 +406,8 @@ class RenameAnalyzeUtil {
 
 		for (int i= 0; i < analyzePackages.length; i++) {
 			ASTNode enclosing= getEnclosingBlockOrMethod(analyzePackages[i].fDeclarationEdit, cuChange, newCUNode);
+			if(enclosing == null)
+				enclosing = newCUNode;
 
 			// get new declaration
 			IRegion newRegion= RefactoringAnalyzeUtil.getNewTextRange(analyzePackages[i].fDeclarationEdit, cuChange);
